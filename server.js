d3f15c4ff5c22e27587ad35e41be449164a729d3
mariomelchor <mario@mariomelchor.com>
2017-09-22 22:42:28 -0700

    Added environment port

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
-
+var exphbs = require('express-handlebars');
+var routes = require("./controllers/burgers_controllers.js");
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,12 +12,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
-var exphbs = require('express-handlebars');
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-var routes = require("./controllers/burgers_controllers.js");
 
 app.use("/", routes);
 
