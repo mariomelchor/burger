a457e0b945fd0d464aadb943cbb40435bc881572
mariomelchor <mario@mariomelchor.com>
2017-09-22 22:46:10 -0700

    Minor Updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
-var routes = require("./controllers/burgers_controllers.js");
+var routes = require('./controllers/burgers_controllers.js');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -10,12 +10,12 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-app.use("/", routes);
+app.use('/', routes);
 
 // Initiate the listener.
 app.listen(port);
